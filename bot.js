9d23ed0b3da20f116f2dfdf21b90df7ad76ca78a
Bakdashinigami <31391450+Bakdashinigami@users.noreply.github.com>
2017-08-27 16:32:34 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,3 +6,6 @@ var voiceChannelName = "General";
 var botToken = "MzUxNDc4NjA3ODYxNzc2Mzg0.DITLeg.zFfwwT25abFUY6Rmc8n8oaRHEVc";
 
 bot.run(serverName, textChannelName, voiceChannelName, botToken);
+
+bot.setYoutubeKey("AIzaSyA1u3vHOwAyaj6o78f3woAB8eegtMippvQ
+");

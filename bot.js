d9f00d91cb1e309f32f6995debcb3e31cfb8c79a
Bakdashinigami <31391450+Bakdashinigami@users.noreply.github.com>
2017-08-27 16:31:22 -0500

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,8 @@
+var bot = require("discord-music-bot");
+
+var serverName = "cjdjejejek";
+var textChannelName = "general";
+var voiceChannelName = "General";
+var botToken = "MzUxNDc4NjA3ODYxNzc2Mzg0.DITLeg.zFfwwT25abFUY6Rmc8n8oaRHEVc";
+
+bot.run(serverName, textChannelName, voiceChannelName, botToken);
